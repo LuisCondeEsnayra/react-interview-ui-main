ffe8dd05314b9b628dff55d7785199885c519c34
luis conde <luisesnayra@gmail.com>
2024-06-11 18:03:28 -0600

    recator revert widget display logic and use Editable widget

--- src/components/EditableWidget/index.js
+++ src/components/EditableWidget/index.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react"; 
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import WidgetInput from "../utils/WidgetInput";
+import { updateWidget } from "../../lib/apiConnect";
 
-const EditableWidget = ({ description, price, onDescriptionChange, onPriceChange }) => {
+const EditableWidget = ({ widget, refreshWidgets, handleCancel }) => {
+  
+  const { description, name, price } = widget;
+  const [newDescription, setNewDescription] = useState(description);
+  const [newPrice, setNewPrice] = useState(price);
+
+  
   const descriptionAttr = { min: 1, max: 1000, text: "Description" };
   const priceAttr = { min: 1, max: 20000, text: "Price" };
 
   const handleDescriptionChange = (e) => {
     const value = e.target.value;
     if (value.length <= descriptionAttr.max) {
-      onDescriptionChange(value);
+      setNewDescription(value);
     }
   };
 
@@ -19,23 +32,46 @@ const EditableWidget = ({ description, price, onDescriptionChange, onPriceChange
         value.match(/^\d{1,}(\.\d{0,2})?$/)) ||
       value === ""
     ) {
-      onPriceChange(value);
+      setNewPrice(value);
     }
   };
 
-  return (
-    <>
-      <WidgetInput
-        attributes={priceAttr}
-        value={price}
-        handleChange={handlePriceChange}
-      />
-      <WidgetInput
-        attributes={descriptionAttr}
-        value={description}
-        handleChange={handleDescriptionChange}
-      />
-    </>
+  const handleSubmit=()=>{ 
+    updateWidget({name,description: newDescription, price:newPrice}).catch((error) =>
+        console.error("Error updating widget", error)
+    );
+    refreshWidgets()
+  }
+
+  return (   <Grid item xs={6}>
+    <Card>
+        <CardContent>
+            <Stack spacing={2}>
+                <Typography component="div" gutterBottom variant="h4">
+                    {name}
+                </Typography> 
+                <WidgetInput
+                  attributes={priceAttr}
+                  value={newPrice}
+                  handleChange={handlePriceChange}
+                />
+                <WidgetInput
+                  attributes={descriptionAttr}
+                  value={newDescription}
+                  handleChange={handleDescriptionChange}
+                />
+
+            </Stack>
+
+            <Button onClick={handleSubmit} variant="contained">
+                Submit
+            </Button>
+            <Button onClick={handleCancel} variant="contained" color="error">
+                Cancel
+            </Button>
+        </CardContent>
+    </Card>
+</Grid> 
   );
 };
 
--- src/components/WidgetDisplay/index.js
+++ src/components/WidgetDisplay/index.js
@@ -1,79 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import EditableWidget from "../EditableWidget";
-import { deleteWidget, updateWidget } from "../../lib/apiConnect";
 
-const DisplayWidget = ({ widget, onUpdateWidgets}) => {
+const DisplayWidget = ({ widget ,handleDelete, handleEdit}) => {
   const { description, name, price } = widget;
-  const [newDescription, setNewDescription] = useState(description);
-  const [newPrice, setNewPrice] = useState(price);
-  const [edit, setEdit] = useState(false);
-  const handleEdit = () => {
-    setEdit(true);
-  };
-  const onDescriptionChange= (value)=>{
-    
-    setNewDescription(value);
-  }
-  const onPriceChange= (value)=>{
-    setNewPrice(value)
-  } 
-  
-
-  const handleEditSubmit=()=>{
-    
-    updateWidget({name,description:newDescription, price:newPrice}).catch((error) =>
-      console.error("Error updating widget", error)
-    );
-    setEdit(false);
-    onUpdateWidgets()
-  }
-  const handleCancel = () => {
-    setEdit(false);
-  };
-  const handleDelete = () => { 
-    deleteWidget(name).catch((error) =>
-      console.error("Error deleting widget", error)
-    );
-    onUpdateWidgets()
-  };
-  const displayFields = (
-    <>
-      <Typography component="div" gutterBottom variant="h5">
-        ${price}
-      </Typography>
-      <Typography color="text.secondary" variant="body2">
-        {description}
-      </Typography>
-    </>
-  );
-
-  const displayButtons = (
-    <>
-      <Button onClick={handleEdit} variant="contained">
-        Edit
-      </Button>
-      <Button onClick={handleDelete} variant="contained" color="error">
-        Delete
-      </Button>
-    </>
-  );
-
-  const editButtons = (
-    <>
-      <Button onClick={handleEditSubmit} variant="contained">
-        Submit
-      </Button>{" "}
-      <Button onClick={handleCancel} variant="contained" color="error">
-        cancel
-      </Button>
-    </>
-  );
+ 
+ 
   return (
     <Grid item xs={6}>
       <Card>
@@ -82,18 +18,19 @@ const DisplayWidget = ({ widget, onUpdateWidgets}) => {
             <Typography component="div" gutterBottom variant="h4">
               {name}
             </Typography>
-            {edit ? (
-              <EditableWidget
-                description={newDescription}
-                price={newPrice}
-                onPriceChange= {onPriceChange}
-                onDescriptionChange={onDescriptionChange}
-              />
-            ) : (
-              displayFields
-            )}
+            <Typography component="div" gutterBottom variant="h5">
+              ${price}
+            </Typography>
+            <Typography color="text.secondary" variant="body2">
+              {description}
+            </Typography>
           </Stack>
-          {edit ? editButtons : displayButtons}
+          <Button onClick={handleEdit} variant="contained">
+            Edit
+          </Button>
+          <Button onClick={handleDelete} variant="contained" color="error">
+            Delete
+          </Button>
         </CardContent>
       </Card>
     </Grid>
